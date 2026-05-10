4bbd75feae0da4a18aaaac51e644596524ad3e2b
ryan <trongdo3110@gmail.com>
2026-05-10 22:42:05 +0700

    chore(config): add tooling, CI, and editor configuration

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { ignores: ['**/node_modules/**', '**/dist/**', '**/.changeset/**'] },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { project: true },
+    },
+    plugins: { '@typescript-eslint': tsPlugin },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    files: ['packages/react/**/*.{ts,tsx}'],
+    plugins: { react: reactPlugin, 'react-hooks': reactHooksPlugin },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];
